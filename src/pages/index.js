1f5234e5bf3f6f5f5c4eee2eb333a059344754b3
santiserena <santiserena@outlook.com.ar>
2022-08-03 12:31:26 -0300

    dev.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+import { Section } from '../styles/GlobalComponents'
 
 const Home = () => {
   return (
