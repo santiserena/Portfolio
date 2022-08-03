9a85f6acdf94d31cfeeecac4bd96c5a3c26a0184
santiserena <santiserena@outlook.com.ar>
2022-08-03 20:40:57 -0300

    taked image out

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,6 +32,7 @@ export default class MyDocument extends Document {
       <Html lang='en-GB'>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <title>Santiago Serena</title>
         </Head>
         <body>
           <Main />
