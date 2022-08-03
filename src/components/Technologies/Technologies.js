5913046721f31c84b4f7e363b544d656e4151947
santiserena <santiserena@outlook.com.ar>
2022-08-03 13:38:58 -0300

    Technologies and timeline

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact,  DiPostgresql,DiJavascript1, DiNodejsSmall } from 'react-icons/di';
+import {SiRedux, SiSequelize, SiExpress} from 'react-icons/si';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
@@ -8,47 +9,47 @@ const Technologies = () =>  (
     <SectionDivider divider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with a range a technologies in the web development world.
-      From Back-end To Design
+      I've worked  from client to server, with many technologies in the web development world.
     </SectionText>
     <List>
+
       <ListItem>
         <picture>
-          <DiReact size="3rem" />
+          <DiJavascript1 style={{marginLeft: "1rem"}} size="3rem"></DiJavascript1>
+          <DiReact style={{marginLeft: "1rem"}} size="3rem" />
+          <SiRedux style={{marginLeft: "1.5rem", marginBottom: "0.5rem"}} size="2rem"/>
+          <br/><br/>
         </picture>
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
-            Experiece with <br />
-            React.js
+            JavaScript <br/>
+            React.js <br/>
+            Redux
           </ListParagraph>
         </ListContainer>
       </ListItem>
+
       <ListItem>
         <picture>
-          <DiFirebase size="3rem" />
+          <DiNodejsSmall style={{marginLeft: "1rem"}} size="3rem" />
+          <SiSequelize style={{marginLeft: "1.3rem", marginBottom:"0.2rem"}} size="2.5rem" />
+          <SiExpress style={{marginLeft: "1.7rem", marginBottom:"0.2rem"}} size="2.5rem" />
+          <br/><br/>
         </picture>
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
-            Experience with <br />
-            Node and Databases
+            Node.Js <br/>
+            Express <br/>
+            Sequelize
           </ListParagraph>
         </ListContainer>
       </ListItem>
-      <ListItem>
-        <picture>
-          <DiZend size="3rem" />
-        </picture>
-        <ListContainer>
-          <ListTitle>UI/UX</ListTitle>
-          <ListParagraph>
-            Experience with <br />
-            tools like Figma
-          </ListParagraph>
-        </ListContainer>
-      </ListItem>
+
+
     </List>
+      <br/><br/><br/>
     <SectionDivider colorAlt />
   </Section>
 );
